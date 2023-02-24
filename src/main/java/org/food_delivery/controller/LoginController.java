341c450fd3c0f78f1485a086a82fa7babc9f558d
Varga Mark <mrk_varga@yahoo.com>
2023-02-24 22:12:39 +0200

    login page

---
+++ src/main/java/org/food_delivery/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.food_delivery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+}
