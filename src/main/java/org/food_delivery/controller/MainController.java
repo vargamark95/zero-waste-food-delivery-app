f26f0adb869c470b5f36eafb235b03efbfb3316b
Varga Mark <mrk_varga@yahoo.com>
2023-02-26 23:01:18 +0200

    added single product view function

--- src/main/java/org/food_delivery/controller/MainController.java
+++ src/main/java/org/food_delivery/controller/MainController.java
@@ -5,9 +5,11 @@ import org.food_delivery.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -17,20 +19,40 @@ public class MainController {
     private ProductService productService;
 
     @GetMapping("/addProduct")
-    public String addFoodItemGet(Model model){
+    public String addFoodItemGet(Model model) {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "addProduct";
     }
 
     @PostMapping("/addProduct")
-    public String addFoodItemPost(@ModelAttribute ProductDto productDto){
-        productService.addProduct(productDto);
+    public String addFoodItemPost(@ModelAttribute ProductDto productDto,
+                                  @RequestParam("productImage") MultipartFile multipartFile) {
+        productService.addProduct(productDto, multipartFile);
         return "addProduct";
     }
 
+    @GetMapping("/home")
+    public String getHome(Model model) {
+        List<ProductDto> productDtos = productService.findAllProductDtos();
+        model.addAttribute("productDtos", productDtos);
+        return "home";
+    }
+
+    @GetMapping("/product/{productId}")
+    public String viewProductGet(@PathVariable(value = "productId") String productId, Model model) {
+        System.out.println("Am dat click pe produsul cu id-ul: " + productId);
+        Optional<ProductDto> optionalProductDto = productService.optionalFindProductDtoById(productId);
+        if(optionalProductDto.isEmpty()){
+            return "error";
+        }
+        ProductDto productDto = optionalProductDto.get();
+        model.addAttribute("productDto", productDto);
+        return "viewProduct";
+    }
+
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin() {
         return "login";
     }
 }
--- src/main/java/org/food_delivery/service/ProductService.java
+++ src/main/java/org/food_delivery/service/ProductService.java
@@ -6,6 +6,11 @@ import org.food_delivery.mapper.ProductMapper;
 import org.food_delivery.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,9 +19,36 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public void addProduct(ProductDto productDto){
-        Product product = productMapper.map(productDto);
+    public void addProduct(ProductDto productDto, MultipartFile productImage){
+        Product product = productMapper.map(productDto, productImage);
         productRepository.save(product);
+
+
+    }
+
+    public List<ProductDto> findAllProductDtos() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+        for(Product product : products){
+            ProductDto productDto = productMapper.map(product);
+            productDtos.add(productDto);
+        }
+        return productDtos;
+    }
+
+    public Optional<ProductDto> optionalFindProductDtoById(String productId) {
+        try{
+            Optional<Product> optionalProduct = productRepository.findById(Integer.valueOf(productId));
+            if(optionalProduct.isEmpty()){
+                return Optional.empty();
+            }
+            Product product = optionalProduct.get();
+            ProductDto productDto = productMapper.map(product);
+            return Optional.of(productDto);
+        }catch (NumberFormatException e){
+            return Optional.empty();
+        }
+
 
 
     }
